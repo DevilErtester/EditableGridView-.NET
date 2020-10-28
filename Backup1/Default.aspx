f81940619be30d97dcf746162f666461b22d0c37
DevilErtester <xX1mario1Xx63@gmail.com>
2020-10-28 11:03:03 +0100

    base

---
+++ Backup1/Default.aspx
@@ -0,0 +1,140 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="EditableGridview._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Diferentes Controls ASP.NET - Gridview</title>
+</head>
+<body>
+    <form id="frmeditableGridview" runat="server">
+    <div style="text-align: center">
+        <h4>
+            Informaci&oacute;n del empleado</h4>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CellPadding="4"
+            ForeColor="#333333" GridLines="None" HorizontalAlign="Center" OnRowCancelingEdit="GridView1_RowCancelingEdit"
+            OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating" OnRowDeleting="GridView1_RowDeleting" 
+            OnRowDataBound="GridView1_RowDataBound" ShowFooter="true" OnRowDeleted="GridView1_Deleted">
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+            <Columns>
+                <asp:TemplateField HeaderText="ID">
+                    <ItemTemplate>
+                        <asp:Label ID="Label" runat="server" Text='<%# Eval("emp_id") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+
+                <asp:TemplateField HeaderText="Nombre">
+                    <ItemTemplate>
+                        <asp:Label ID="Label2" runat="server" Text='<%# Eval("emp_name") %>'></asp:Label>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="TextBox1" runat="server" Text='<%# Eval("emp_name")%>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="XC1" runat="server"></asp:TextBox>
+                    </FooterTemplate>                    
+                </asp:TemplateField>
+
+
+                <asp:TemplateField HeaderText="Direcci&oacute;n">
+                    <ItemTemplate>
+                        <asp:Label ID="Label3" runat="server" Text='<%# Eval("emp_address") %>'></asp:Label>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="TextBox2" runat="server" Text='<%# Eval("emp_address") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="XC2" runat="server"></asp:TextBox>
+                    </FooterTemplate>                                        
+                </asp:TemplateField>
+
+
+                <asp:TemplateField HeaderText="Departamento">
+                    <ItemTemplate>
+                        <asp:Label ID="Label4" runat="server" Text='<%# Eval("department") %>'></asp:Label>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:DropDownList ID="DropDownList1" runat="server">
+                        </asp:DropDownList>
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:DropDownList ID="DropDownList1" runat="server" />
+                    </FooterTemplate>                                        
+                </asp:TemplateField>
+
+
+                <asp:TemplateField HeaderText="Salario">
+                    <ItemTemplate>
+                        <asp:Label ID="Label5" runat="server" Text='<%# Eval("salary") %>'></asp:Label>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:TextBox ID="TextBox3" runat="server" Text='<%# Eval("salary") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:TextBox ID="XC4" runat="server"></asp:TextBox>
+                    </FooterTemplate>                                        
+                </asp:TemplateField>
+
+
+                <asp:TemplateField HeaderText="Estado civil">
+                    <ItemTemplate>
+                        <asp:Label ID="Label6" runat="server" Text='<%# Eval("Maritalstatus") %>'></asp:Label>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+                            <asp:ListItem>Soltero</asp:ListItem>
+                            <asp:ListItem>Casado</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </EditItemTemplate>
+                    <FooterTemplate>
+                        <asp:RadioButtonList ID="XC5" runat="server">
+                            <asp:ListItem>Soltero</asp:ListItem>
+                            <asp:ListItem>Casado</asp:ListItem>
+                        </asp:RadioButtonList>                       
+                    </FooterTemplate>                                        
+                </asp:TemplateField>
+
+
+                <asp:TemplateField HeaderText="Estatus">
+                    <ItemTemplate>
+                        <asp:Label ID="Label7" runat="server" Text='<%# Eval("Active_Status") %>'></asp:Label>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:CheckBoxList ID="CheckBoxList2" runat="server">
+                            <asp:ListItem>Activo</asp:ListItem>
+                            <asp:ListItem>Inactivo</asp:ListItem>
+                        </asp:CheckBoxList>
+                    </EditItemTemplate>
+                </asp:TemplateField>
+
+
+                <asp:TemplateField HeaderText="Editar" ShowHeader="false">
+                    <ItemTemplate>
+                        <asp:LinkButton ID="btnedit" runat="server" CommandName="Edit" Text="Editar"></asp:LinkButton>
+                    </ItemTemplate>
+                    <EditItemTemplate>
+                        <asp:LinkButton ID="btnupdate" runat="server" CommandName="Update" Text="Actualizar"></asp:LinkButton>
+                        <asp:LinkButton ID="btncancel" runat="server" CommandName="Cancel" Text="Cancelar"></asp:LinkButton>
+                    </EditItemTemplate>
+                </asp:TemplateField>
+
+                <asp:TemplateField HeaderText="Eliminar" ShowHeader="false">
+                    <ItemTemplate>                    
+                        <asp:LinkButton ID="btnDelete" runat="server" CommandName="Delete" Text="Eliminar"></asp:LinkButton>
+                    </ItemTemplate>
+                    <FooterTemplate>
+                        <asp:button  ID="sNew" Text="Enviar" runat="server" OnClick="addItem"></asp:button>
+                    </FooterTemplate>                    
+               </asp:TemplateField>
+            </Columns>
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+            <EditRowStyle BackColor="#CCCCCC" />
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Backup1/Default.aspx.cs
@@ -0,0 +1,182 @@
+﻿// DataView.CommandName: http://msdn.microsoft.com/es-es/library/system.web.ui.webcontrols.gridview.rowcommand(v=vs.80).aspx
+// DataView http://msdn.microsoft.com/es-es/library/system.web.ui.webcontrols.gridview.onrowdatabound(v=vs.80).aspx 
+// DataView.RowState http://msdn.microsoft.com/es-es/library/system.web.ui.webcontrols.gridviewrow.rowstate.aspx
+
+
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Xml;
+using System.Web.UI.WebControls.WebParts;
+
+
+
+/*
+ O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O
+O|0                   Provider:Microsoft.ACE.OLEDB.12.0                       0|O
+o|o                             Bb0y ch3Ché                                   o|o
+0|O                        chepseyer@gmail.com                                O|0
+O|0                       bboy_rys@hotmail.com                                0|O
+o|o                     --> 18 - Marzo - 2011 <--                             o|o
+0|O           Microsoft Visual Studio - Microsoft Access 2007                 O|0
+ O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O_0_o_O
+*/
+namespace EditableGridview
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        string con = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+        OleDbConnection sqlcon;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                showgrid();
+            }            
+        }
+        public void showgrid()
+        {
+            DataTable dt = new DataTable();
+            sqlcon = new OleDbConnection(con);
+            sqlcon.Open();
+            string strQuery = "SELECT * FROM employee ";
+
+            OleDbDataAdapter sda = new OleDbDataAdapter(strQuery, con);
+            
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            showgrid();
+        }
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+           GridView1.EditIndex = e.NewEditIndex;
+           showgrid();
+        }
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            Label lb = (Label)GridView1.Rows[e.RowIndex].FindControl("Label");
+            DropDownList ddl = (DropDownList)GridView1.Rows[e.RowIndex].FindControl("DropDownList1");
+            RadioButtonList rbl = (RadioButtonList)GridView1.Rows[e.RowIndex].FindControl("RadioButtonList1");
+            CheckBoxList chb = (CheckBoxList)GridView1.Rows[e.RowIndex].FindControl("CheckBoxList2");            
+            TextBox tx1 = (TextBox)GridView1.Rows[e.RowIndex].FindControl("TextBox1");
+            TextBox tx2 = (TextBox)GridView1.Rows[e.RowIndex].FindControl("TextBox2");
+            TextBox tx3 = (TextBox)GridView1.Rows[e.RowIndex].FindControl("TextBox3");
+            sqlcon = new OleDbConnection(con);
+            sqlcon.Open();
+            string sql = "UPDATE employee SET Emp_name='" + tx1.Text + "',Emp_address='" + tx2.Text + "',Department='" +
+                ddl.SelectedValue.ToString() + "',Salary='" + tx3.Text + "',Maritalstatus='" +
+                rbl.SelectedValue.ToString() + "',Active_Status='" + chb.SelectedValue.ToString() + "' WHERE Emp_ID=" +
+                lb.Text;
+
+            OleDbCommand cmd = new OleDbCommand(sql);
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlcon;
+            cmd.ExecuteNonQuery();
+            GridView1.EditIndex = -1;
+            sqlcon.Close();
+            showgrid();
+        }
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            
+            Label lb = (Label)GridView1.Rows[e.RowIndex].FindControl("Label");
+            sqlcon = new OleDbConnection(con);
+            sqlcon.Open();
+            string sql = "DELETE FROM employee where Emp_ID=" + lb.Text;
+            OleDbCommand cmd = new OleDbCommand(sql);
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlcon;
+            cmd.ExecuteNonQuery();
+            sqlcon.Close();
+            GridView1.EditIndex = -1;
+            showgrid();
+            
+        }
+        protected void GridView1_Deleted (object sender, GridViewDeletedEventArgs e) {
+            String s = "";
+        }
+
+
+        public DataTable load_department()
+        {
+            DataTable dt = new DataTable();
+            sqlcon = new OleDbConnection(con);
+            sqlcon.Open();
+            string sql = "SELECT dept_name FROM department";
+            OleDbCommand cmd = new OleDbCommand(sql);
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlcon;
+            OleDbDataAdapter sd = new OleDbDataAdapter(cmd);
+            sd.Fill(dt);
+            return dt;
+        }
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            DataRowView drv = e.Row.DataItem as DataRowView;
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {                
+                if  ((e.Row.RowState & DataControlRowState.Edit) > 0)
+                {
+                    DropDownList dp = (DropDownList)e.Row.FindControl("DropDownList1");
+
+                    //dp.DataSource = load_department();
+                    //dp.DataBind();
+
+                    
+                    DataTable dt = load_department();
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        ListItem lt = new ListItem();
+                        lt.Text = dt.Rows[i][0].ToString();
+                        dp.Items.Add(lt);
+                    }
+
+                    // dp.SelectedValue = drv[3].ToString();
+                    dp.SelectedValue = e.Row.Cells[3].ToString();
+
+                    RadioButtonList rbtnl = (RadioButtonList)e.Row.FindControl("RadioButtonList1");
+                    rbtnl.SelectedValue = drv[5].ToString();
+                    CheckBoxList chkb = (CheckBoxList)e.Row.FindControl("CheckBoxList2");
+                    chkb.SelectedIndex = -1;
+                    chkb.SelectedValue = drv[6].ToString();
+                }
+            }
+
+            // Pel Footer
+            if (e.Row.RowType == DataControlRowType.Footer)
+            {
+                DropDownList dp = (DropDownList)e.Row.FindControl("DropDownList1");
+                DataTable dt = load_department();
+                for (int i = 0; i < dt.Rows.Count; i++) {
+                    ListItem lt = new ListItem();
+                    lt.Text = dt.Rows[i][0].ToString();
+                    dp.Items.Add(lt);
+                 }
+            }
+        }
+        protected void addItem(object sender, EventArgs e)
+        {
+            TextBox B = (TextBox)GridView1.FooterRow.FindControl("XC1");
+            String XC1 = B.Text.ToString();
+
+            B = (TextBox)GridView1.FooterRow.FindControl("XC2");
+            String XC2 = B.Text.ToString();
+
+          
+        }
+
+    }
+}
+
